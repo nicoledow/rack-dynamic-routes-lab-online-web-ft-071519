034d50ffad81463d58cf82e33ee270409bab9f99
Nicole Dow <nikki.dow71@gmail.com>
2019-08-25 20:00:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,5 @@
-class Application 
+class Application
   
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-    
-    if req.path.match(/items/)
-      requested_item = req.path.split("/items/").last
-      if Item.all.find {|i| i.name == requested_item}
-        obj = Item.all.find {|i| i.name == requested_item}
-        resp.write "#{obj.price}"
-      else
-        resp.write "We don't have that item"
-      end
-    else
-      resp.status = 404
-      resp.write "Route not found"
-    end
-    
-  end
   
   
 end
