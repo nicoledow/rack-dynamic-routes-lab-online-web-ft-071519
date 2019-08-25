824849f039a40547ec82d690d6da1c7d48af4e2d
Nicole Dow <nikki.dow71@gmail.com>
2019-08-25 19:54:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
     
     if req.path.match(/items/)
       requested_item = req.path.split("/items/").last
+      if Item.all.find {|i| i.name == requested_item}
     else
       resp.write "Item not found"
       resp.status = 404
