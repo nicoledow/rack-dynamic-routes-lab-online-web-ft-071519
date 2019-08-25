374632f882d3a14b0f1f458cf407d3ac4dbd4d59
Nicole Dow <nikki.dow71@gmail.com>
2019-08-25 20:00:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      
+    end
+    
+    
+    
   end
   
 end
